9592e3f69b0a60b89aa439f6205bb2afd37c80ad
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-03-23 15:11:56 +0800

    improve expense item2

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,16 +3,12 @@ import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from "./ExpenseDate";
  const ExpenseItem=(props)=> {
-  const [title,setTitle]=useState(props.title);
-  const clickHandler =()=>{
-    setTitle('Updated')
-  }
   return (
     <li>
       <Card className="expense-item">
         <ExpenseDate date={props.date}></ExpenseDate>
         <div className="expense-item__description">
-          <h2>{title}</h2> 
+          <h2>{props.title}</h2> 
           <div className="expense-item__price">${props.amount}</div>
         </div>
       </Card>
