955dbf869a419de0ba8d29caa47067bec1b55ca2
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-03-23 11:56:57 +0800

    Adding a Chart

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,7 +6,6 @@ import ExpenseDate from "./ExpenseDate";
 
  const ExpenseItem=(props)=> {
   const [title,setTitle]=useState(props.title);
-  
   const clickHandler =()=>{
     setTitle('Updated')
   }
